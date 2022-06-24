26e406c1952bb8c626f586d13943aabac045d105
Michał Walasiak <michal.walasiak@justeattakeaway.com>
2022-06-24 16:12:19 +0200

    Initial commit

---
+++ app/src/main/java/com/mwalasiak/pyramid/LevelsView.kt
@@ -0,0 +1,13 @@
+package com.mwalasiak.pyramid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LevelsView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_levels_view)
+
+
+    }
+}
