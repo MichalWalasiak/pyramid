957989c2e2cc2aaeb10d1363aa6d625760eb9052
Michał Walasiak <michal.walasiak@justeattakeaway.com>
2022-06-30 09:48:23 +0200

    seting new activity

--- app/src/main/java/com/mwalasiak/pyramid/LevelsActivity.kt
+++ app/src/main/java/com/mwalasiak/pyramid/LevelsActivity.kt
@@ -3,7 +3,7 @@ package com.mwalasiak.pyramid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LevelsView : AppCompatActivity() {
+class LevelsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_levels_view)
--- app/src/main/java/com/mwalasiak/pyramid/MainActivity.kt
+++ app/src/main/java/com/mwalasiak/pyramid/MainActivity.kt
@@ -3,15 +3,18 @@ package com.mwalasiak.pyramid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mwalasiak.pyramid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val intent = Intent(this, LevelsView::class.java)
-        startActivity(intent)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.addSetButton.setOnClickListener {
+            val intent = Intent(this, LevelsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
