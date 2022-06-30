dd4547b7eaa0c5c3ae2dc8ce64a0fc4b3edb930d
Michał Walasiak <michal.walasiak@justeattakeaway.com>
2022-06-30 14:41:04 +0200

    add views

--- app/src/main/java/com/mwalasiak/pyramid/LevelsActivity.kt
+++ app/src/main/java/com/mwalasiak/pyramid/LevelsActivity.kt
@@ -2,11 +2,16 @@ package com.mwalasiak.pyramid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mwalasiak.pyramid.databinding.ActivityLevelsViewBinding
 
 class LevelsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLevelsViewBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_levels_view)
+        binding = ActivityLevelsViewBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
 
     }
