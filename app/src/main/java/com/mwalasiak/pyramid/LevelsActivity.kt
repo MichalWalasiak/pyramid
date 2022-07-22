97de868bfe6607cc10c36d62010c1ce5b351b012
Michał Walasiak <michal.walasiak@justeattakeaway.com>
2022-07-22 17:27:27 +0200

    ading levels with numbers

--- app/src/main/java/com/mwalasiak/pyramid/LevelsActivity.kt
+++ app/src/main/java/com/mwalasiak/pyramid/LevelsActivity.kt
@@ -1,5 +1,6 @@
 package com.mwalasiak.pyramid
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mwalasiak.pyramid.databinding.ActivityLevelsViewBinding
@@ -12,7 +13,43 @@ class LevelsActivity : AppCompatActivity() {
         binding = ActivityLevelsViewBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        binding.addNumber.setOnClickListener { readNumbersFromEditText() }
+        binding.seeLevel.setOnClickListener { showNumbersFromLevel(R.id.firstLevel) }
+    }
 
+    private fun readNumbersFromEditText() {
+        val latestDraw: MutableList<String>
+        val delim = ","
+        val text = binding.enterNumber.text
+        writeToSharedPreferences(R.id.firstLevel, text.toString())
+        val readed = readFromSharedPreferences(R.id.firstLevel)
+        latestDraw = listOf(readed?.split(delim).toString()).toMutableList()
+        binding.firstLevel.text = latestDraw.toString().replace(",", " ").removeSurrounding("[[", "]]")
+    }
+
+    private fun writeToSharedPreferences(fileKey: Int, text: String) {
+        val sharedPref = getSharedPreferences("level", Context.MODE_PRIVATE) ?: return
+        with(sharedPref.edit()) {
+            putString(getString(fileKey), text)
+            apply()
+        }
+
+
+    }
+
+    private fun readFromSharedPreferences(fileKey: Int): String? {
+        val sharedPref = getSharedPreferences("level", Context.MODE_PRIVATE)
+        return sharedPref.getString(getString(fileKey), null)
+    }
+
+    private fun showNumbersFromLevel(level: Int) {
+        val readed = readFromSharedPreferences(level)
+        val latestDraw = listOf(readed?.split(","))
+        binding.firstLevel.text = latestDraw.toString().replace(",", "//").removeSurrounding("[[", "]]")
+
+    }
+
+    private fun compareLevels() {
 
     }
 }
